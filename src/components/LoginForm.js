ba39504a8a0aca88d571ae4eaea95e75b763726d
marimbotico <isaacfh@bu.edu>
2024-07-21 11:57:41 -0600

    second commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,12 +9,12 @@ export default class LoginForm extends React.Component {
                 <form>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input type="username" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="username" className="form-control" id="username" placeholder="Enter email" />
                         <small id="usernameHelp" className="form-text text-muted">We'll never share your username with anyone else.</small>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" id="password" placeholder="Password" />
                     </div>
                     <SubmitButton />
                 </form>
